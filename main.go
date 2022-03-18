5aa039068ca1d1549ec242bddd9a9f9146b2f896
BigCubeCat <bigcubecat@yandex.ru>
2022-03-18 20:59:22 +0700

    server package

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
 	"potatoDB/core"
+	"potatoDB/server"
 )
-
-type PostJSON struct {
-	Cluster string
-	ID      string
-	Data    string
-}
-
-type GetJSON struct {
-	Cluster string
-	ID      string
-}
 
 func main() {
 	err := godotenv.Load()
@@ -28,66 +16,13 @@ func main() {
 	}
 
 	dirName := os.Getenv("DIR_NAME")
+	address := os.Getenv("ADDRESS")
 	if startError := core.Init(dirName); startError != nil {
 		log.Fatal("Error: directory doesnt exists")
 	}
-	http.HandleFunc("/", handler)
-	// URL: http://127.0.0.1:8080/
-	err = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", server.HttpHandler)
+	err = http.ListenAndServe(":"+address, nil)
 	if err != nil {
 		log.Fatal(err)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		var data GetJSON
-		var value []byte
-
-		// Decoding get request data
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			log.Fatal(err)
-		}
-		// Getting value with API
-		value, err = core.DBCore.Get(data.Cluster, data.ID)
-		if err != nil {
-			log.Fatalf("API error: %s", err)
-		}
-		// Sending value back
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-		err = json.NewEncoder(w).Encode(string(value))
-		if err != nil {
-			log.Fatal(err)
-		}
-	case "POST":
-		var data PostJSON
-		// Decoding post request data
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			fmt.Println(err)
-		}
-		// Using API
-		if data.ID == "" {
-			var id string
-			id, err = core.DBCore.Add(data.Cluster, []byte(data.Data))
-			if err != nil {
-				log.Fatalf("API error: %s", err)
-			}
-			fmt.Printf("New ID: %s\n", id)
-		} else {
-			err = core.DBCore.Set(data.Cluster, data.ID, []byte(data.Data))
-			if err != nil {
-				log.Fatalf("API error: %s", err)
-			}
-		}
-	case "DELETE":
-		fmt.Println(r.Method)
-	default:
-		fmt.Println("Wrong method!")
 	}
 }
