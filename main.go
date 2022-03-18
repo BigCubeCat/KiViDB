0aa60332f36726280a31f0631dbd758c8a6fa97f
Kirill Parkhomenko <74421236+KXRXH@users.noreply.github.com>
2022-03-18 21:30:40 +0700

    Log update & Some fixes.

--- main.go
+++ main.go
@@ -14,12 +14,21 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	dirName := os.Getenv("DIR_NAME")
 	address := os.Getenv("ADDRESS")
+	logFileName := os.Getenv("LOG_FILE")
 	if startError := core.Init(dirName); startError != nil {
 		log.Fatal("Error: directory doesnt exists")
 	}
+	path := logFileName
+	_ = os.Remove(path)
+	f, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Unable to open log file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+	log.Println("This is a test log entry")
 	http.HandleFunc("/", server.HttpHandler)
 	err = http.ListenAndServe(":"+address, nil)
 	if err != nil {
--- server/server.go
+++ server/server.go
@@ -29,19 +29,20 @@ func HttpHandler(w http.ResponseWriter, r *http.Request) {
 		err := json.NewDecoder(r.Body).Decode(&data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
-			log.Fatal(err)
+			log.Panicf("Decoding error: %v\n", err)
 		}
 		// Getting value with API
 		value, err = core.DBCore.Get(data.Cluster, data.ID)
 		if err != nil {
-			log.Fatalf("API error: %s", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Panicf("API error: %v\n", err)
 		}
 		// Sending value back
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
 		err = json.NewEncoder(w).Encode(string(value))
 		if err != nil {
-			log.Fatal(err)
+			log.Panicf("Encoding error: %v\n", err)
 		}
 	case "POST":
 		var data PostJSON
@@ -49,25 +50,27 @@ func HttpHandler(w http.ResponseWriter, r *http.Request) {
 		err := json.NewDecoder(r.Body).Decode(&data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
-			fmt.Println(err)
+			log.Panicf("Decoding error: %v\n", err)
 		}
 		// Using API
 		if data.ID == "" {
 			var id string
 			id, err = core.DBCore.Add(data.Cluster, []byte(data.Data))
 			if err != nil {
-				log.Fatalf("API error: %s", err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				log.Panicf("API error: %v\n", err)
 			}
-			fmt.Printf("New ID: %s\n", id)
+			fmt.Println(id)
 		} else {
 			err = core.DBCore.Set(data.Cluster, data.ID, []byte(data.Data))
 			if err != nil {
-				log.Fatalf("API error: %s", err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				log.Panicf("API error: %v\n", err)
 			}
 		}
 	case "DELETE":
-		fmt.Println(r.Method)
+		fmt.Println(1)
 	default:
-		fmt.Println("Wrong method! Use only POST, GET and DELETE")
+		log.Panicf("Wrong method: %v\n", r.Method)
 	}
 }
