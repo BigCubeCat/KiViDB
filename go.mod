fcecf79066e1a88ab6f47ea66523e42499f9e5f3
Kirill Parkhomenko <74421236+KXRXH@users.noreply.github.com>
2022-03-18 18:48:44 +0700

    ВСЁ НЕ РАБОТАЕТ

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module potatoDB
 go 1.18
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.4.0
 )
--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 	"os"
 	"potatoDB/core"
 )
+
+type PostJSON struct {
+	Cluster string
+	ID      string
+	Data    []byte
+}
+
+type GetJSON struct {
+	Cluster string
+	ID      string
+}
 
 func main() {
 	err := godotenv.Load()
@@ -16,5 +30,55 @@ func main() {
 	dirName := os.Getenv("DIR_NAME")
 	if startError := core.Init(dirName); startError != nil {
 		log.Fatal("Error: directory doesnt exists")
+	}
+	http.HandleFunc("/", handler)
+	// URL: http://127.0.0.1:8080/
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		var data GetJSON
+		var value []byte
+
+		// Decoding get request data
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			log.Fatal(err)
+		}
+		// Getting value with API
+		value, err = core.DBCore.Get(data.Cluster, data.ID)
+		if err != nil {
+			log.Fatalf("API error: %s", err)
+		}
+		// Sending value back
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		err = json.NewEncoder(w).Encode(value)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "POST":
+		var data PostJSON
+		// Decoding post request data
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			log.Fatal(err)
+		}
+		// Using API
+		err = core.DBCore.Set(data.Cluster, data.ID, data.Data)
+		if err != nil {
+			log.Fatalf("API error: %s", err)
+		}
+	case "DELETE":
+		fmt.Println(r.Method)
+	default:
+		fmt.Println("Wrong method!")
 	}
 }
