fcaaef10dcde8ae53d83d78653979f1c2b68ed9c
Kirill Parkhomenko <74421236+KXRXH@users.noreply.github.com>
2022-03-18 22:19:32 +0700

    Cluster created Filter update

--- server/cluster.go
+++ server/cluster.go
@@ -7,18 +7,15 @@ import (
 	"potatoDB/core"
 )
 
-type FilterJSON struct {
+type ClusterJSON struct {
 	Cluster string
-	Regex   string
 }
 
-func FilterHandler(w http.ResponseWriter, r *http.Request) {
+func ClusterHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		var (
-			data   FilterJSON
-			values []string
-		)
+		var data ClusterJSON
+		var values []string
 
 		// Decoding get request data
 		err := json.NewDecoder(r.Body).Decode(&data)
@@ -27,7 +24,7 @@ func FilterHandler(w http.ResponseWriter, r *http.Request) {
 			log.Panicf("Decoding error: %v\n", err)
 		}
 		// Getting values with API
-		values, err = core.DBCore.Filter(data.Cluster, data.Regex)
+		values, err = core.DBCore.GetAll(data.Cluster)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			log.Panicf("API error: %v\n", err)
@@ -38,21 +35,6 @@ func FilterHandler(w http.ResponseWriter, r *http.Request) {
 		err = json.NewEncoder(w).Encode(values)
 		if err != nil {
 			log.Panicf("Encoding error: %v\n", err)
-		}
-	case "DELETE":
-		var data GetAndDeleteJSON
-
-		// Decoding get request data
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			log.Panicf("Decoding error: %v\n", err)
-		}
-		// Deleting key and value
-		err = core.DBCore.Delete(data.Cluster, data.ID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Panicf("API error: %v\n", err)
 		}
 	default:
 		log.Panicf("Wrong method: %v\n", r.Method)
