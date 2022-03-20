d6fc0c32270ee87bfc6c5cf863798065d315b1b5
Kirill Parkhomenko <74421236+KXRXH@users.noreply.github.com>
2022-03-20 18:47:52 +0700

    new get

--- server/cluster.go
+++ server/cluster.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
 	"kiviDB/core"
 	"log"
 	"net/http"
@@ -14,41 +13,41 @@ type ClusterJSON struct {
 }
 
 func ClusterHandler(w http.ResponseWriter, r *http.Request) {
-	var data ClusterJSON
-	// Decoding request's data
-	id := strings.TrimPrefix(r.URL.Path, "/cluster/")
-	fmt.Printf("%q\n", strings.Split(id, "+"))
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Printf("Decoding error: %v\n", err)
-	}
-	switch r.Method {
-	case "GET":
-		var values []string
-		values, err = core.DBCore.GetAll(data.Cluster)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			log.Printf("API error: %v\n", err)
+	var (
+		arg string
+		err error
+	)
+	arg = strings.TrimPrefix(r.URL.Path, "/cluster/")
+	if arg == "" {
+		log.Printf("Argument is empty!")
+	} else {
+		switch r.Method {
+		case "GET":
+			var values []string
+			values, err = core.DBCore.GetAll(arg)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				log.Printf("API error: %v\n", err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			err = json.NewEncoder(w).Encode(values)
+			if err != nil {
+				log.Printf("Encoding error: %v\n", err)
+			}
+		case "DELETE":
+			if err = core.DBCore.DropCluster(arg); err != nil {
+				http.Error(w, "API error: "+err.Error(), http.StatusBadRequest)
+				log.Printf("API error: %v\n", err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+		case "POST":
+			if err = core.DBCore.CreateCluster(arg); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				log.Printf("API error: %v\n", err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+		default:
+			log.Printf("Wrong method: %v\n", r.Method)
 		}
-		w.Header().Set("Content-Type", "application/json")
-		err = json.NewEncoder(w).Encode(values)
-		if err != nil {
-			log.Printf("Encoding error: %v\n", err)
-		}
-	case "DELETE":
-		if err = core.DBCore.DropCluster(data.Cluster); err != nil {
-			http.Error(w, "API error: "+err.Error(), http.StatusBadRequest)
-			log.Printf("API error: %v\n", err)
-		}
-		w.Header().Set("Content-Type", "application/json")
-	case "POST":
-		if err = core.DBCore.CreateCluster(data.Cluster); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			log.Printf("API error: %v\n", err)
-		}
-		w.Header().Set("Content-Type", "application/json")
-	default:
-		log.Printf("Wrong method: %v\n", r.Method)
 	}
 }
--- server/core.go
+++ server/core.go
@@ -6,6 +6,7 @@ import (
 	"kiviDB/core"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type PostJSON struct {
@@ -23,27 +24,28 @@ func CoreHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		var (
-			data  GetAndDeleteJSON
 			value []byte
+			err   error
+			data  GetAndDeleteJSON
 		)
 
-		// Decoding get request data
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			log.Printf("Decoding error: %v\n", err)
-		}
-		// Getting value with API
-		value, err = core.DBCore.Get(data.Cluster, data.Id)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Printf("API error: %v\n", err)
-		}
-		// Sending value back
-		w.Header().Set("Content-Type", "application/json")
-		err = json.NewEncoder(w).Encode(string(value))
-		if err != nil {
-			log.Printf("Encoding error: %v\n", err)
+		args := strings.Split(strings.TrimPrefix(r.URL.Path, "/cluster/"), "/")
+		if len(args) == 2 {
+			data = GetAndDeleteJSON{Cluster: args[0], Id: args[0]}
+			// Getting value with API
+			value, err = core.DBCore.Get(data.Cluster, data.Id)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				log.Printf("API error: %v\n", err)
+			}
+			// Sending value back
+			w.Header().Set("Content-Type", "application/json")
+			err = json.NewEncoder(w).Encode(string(value))
+			if err != nil {
+				log.Printf("Encoding error: %v\n", err)
+			}
+		} else {
+			fmt.Printf("Not enough arguments: expected 2, but got: %d", len(args))
 		}
 	case "POST":
 		var data PostJSON
